7f27c2462aac6f35f1ba8527da7973cfae5b7d38
Raleigh Fatoki <fatoki09@yahoo.com>
2013-09-04 15:59:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ghwebsite::Application.config.secret_key_base = '8515d34cd42e7a4b0bf671acbe973382a2cdccd3d36c8dbbaf48a0c9e9c8c92f6d065025b41eb4c776baaa66607121a404d2cecff688d7d1b2dcda3383dbf85f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ghwebsite::Application.config.session_store :cookie_store, key: '_ghwebsite_session'
