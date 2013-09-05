bee18ecf476f2c10293b2c323af867109eea8b51
Raleigh Fatoki <fatoki09@yahoo.com>
2013-09-04 23:25:14 -0500

    added nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Ghwebsite::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'webesite_pages#home'
+  root 'website_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
