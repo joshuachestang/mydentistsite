64443cbc797d92aef551a671a186ffb4dacad086
Joshua Chestang <joshua.chestang@gmail.com>
2013-09-16 15:51:04 -0500

    contact message stuff

---
+++ app/mailers/contact_message_mailer.rb
@@ -0,0 +1,8 @@
+class ContactMessageMailer < ActionMailer::Base
+  default from: "yfatoki@great-heights.com"
+
+   def inbox_message(contact_message)
+	  	@contact_message = contact_message
+	    mail(:from => "yfatoki@great-heights.com", :bcc =>  :subject => "New Message")
+	  end
+end
