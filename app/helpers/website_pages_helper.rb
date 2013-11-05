556bcb7217a1a20ae00512f436f7b74fae62a760
Joshua Chestang <joshua.chestang@gmail.com>
2013-11-05 09:15:29 -0600

    changed time helper for.

--- app/helpers/website_pages_helper.rb
+++ app/helpers/website_pages_helper.rb
@@ -2,20 +2,16 @@ module WebsitePagesHelper
 
 
 	def open_right_now_weekday?
-		if Time.now.strftime('A%') == 'Monday' || 'Wednesday' || 'Friday' 
-			if Time.now.hour >= 9 && Time.now.hour <= 18
-				return true 
-			end
+		if Time.now.strftime("%A") == ('Monday' || 'Wednesday' || 'Friday') && (Time.now.hour >= 9 && Time.now.hour <= 18)
+			return true 
 		else
 			return false
 		end
 	end
 
 	def open_right_now_saturday?
-		if Time.now.strftime('A%') == 'Saturday' 
-			if Time.now.hour >= 8 && Time.now.hour <= 12
+		if Time.now.strftime("%A") == ('Saturday') &&  (Time.now.hour >= 8 && Time.now.hour <= 12)
 				return true 
-			end
 		else
 			return false
 		end
