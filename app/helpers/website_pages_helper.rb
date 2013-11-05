fc31bd2516a135a4fe4b42022dd20b60e4e895ce
Joshua Chestang <joshua.chestang@gmail.com>
2013-11-05 02:13:29 -0600

    added div for current open time.

--- app/helpers/website_pages_helper.rb
+++ app/helpers/website_pages_helper.rb
@@ -1,2 +1,23 @@
 module WebsitePagesHelper
+
+
+	def open_right_now_weekday?
+		if Time.now.strftime('A%') == 'Monday' || 'Wednesday' || 'Friday' 
+			if Time.now.hour >= 9 && Time.now.hour <= 18
+				return true 
+			end
+		else
+			return false
+		end
+	end
+
+	def open_right_now_saturday?
+		if Time.now.strftime('A%') == 'Saturday' 
+			if Time.now.hour >= 8 && Time.now.hour <= 12
+				return true 
+			end
+		else
+			return false
+		end
+	end
 end
