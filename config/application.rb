226b1184fe0f8c7d38c29e6a87033b38e19ae21a
Joshua Chestang <joshua.chestang@gmail.com>
2013-11-08 18:10:06 -0600

    changed timezone

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 module Ghwebsite
   class Application < Rails::Application
 
-    config.time_zone = 'Chicago'
+    config.time_zone = "Central Time (US & Canada)"
     config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
