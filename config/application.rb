c0fa069d111788a5f95ae2335e4ea9a0bc4cbe09
Joshua Chestang <joshua.chestang@gmail.com>
2013-11-08 18:06:19 -0600

    setting Timezone for heroku app.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module Ghwebsite
   class Application < Rails::Application
+
+    config.time_zone = 'Chicago'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
