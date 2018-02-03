8769b26a8313048314e70604f5e424a62597c8a4
Joshua Chestang <joshua.chestang@gmail.com>
2018-02-03 03:49:14 -0600

    rails ruby version adjustment.

--- app/helpers/website_pages_helper.rb
+++ app/helpers/website_pages_helper.rb
@@ -3,15 +3,15 @@ module WebsitePagesHelper
 
 	def open_right_now_weekday?
 		if Time.now.strftime("%A") == ('Monday' || 'Wednesday' || 'Friday') && (Time.now.hour >= 9 && Time.now.hour <= 18)
-			return true 
+			return true
 		else
 			return false
 		end
 	end
 
 	def open_right_now_saturday?
-		if Time.now.strftime("%A") == ('Saturday') &&  (Time.now.hour >= 8 && Time.now.hour <= 12)
-				return true 
+		if Time.now.strftime("%A") == ('Saturday') && (Time.now.hour >= 8 && Time.now.hour <= 12)
+				return true
 		else
 			return false
 		end
